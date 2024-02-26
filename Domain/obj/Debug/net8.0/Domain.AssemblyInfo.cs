e7b6bc2276f3b9ec986c0473513c64dfef580957
Wikrama1 <rakaiwikrama@gmail.com>
2024-02-26 08:03:31 +0700

    permbaruan

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c60d654cf307819e81fa60560e890ece768d4bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd90cf38a42c1e8ca2afa1a4fd69c4b7e7d53818")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
