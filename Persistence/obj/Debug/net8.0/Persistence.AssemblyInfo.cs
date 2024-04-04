fd408e84550a635ef973ff15b71de85730676a97
oktadafa <oktadafasampang@gmail.com>
2024-04-04 11:16:53 +0700

    04/04/2024

--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec7d01b32bf3e9ed43fc1fc2a5f7f163780b7bdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03c306977baa21ac914913906d605ec0677d053")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
